b272437e8d82d3bd36811475f94d89badf3ee48d
Olesya <hurmuzz23@gmail.com>
2022-04-24 12:17:30 +0300

    applied format

--- srcs/config/Config.cpp
+++ srcs/config/Config.cpp
@@ -9,49 +9,39 @@
 
 using namespace config;
 
-Config::Config(const logger::Logger &log) : _log(log)
-{}
+Config::Config(const logger::Logger &log) : _log(log) {}
 
-Config::~Config()
-{}
+Config::~Config() {}
 
-void Config::parse(const std::string &filename)
-{
+void Config::parse(const std::string &filename) {
 	const std::vector<std::string> tokens = this->_lexing(filename);
 	this->_parsing(tokens);
 
 	this->_fill_options(_global_module);
 }
 
-const logger::Options &Config::get_logger() const
-{
+const logger::Options &Config::get_logger() const {
 	return (this->_log_opts);
 }
 
-const server::Options &Config::get_server() const
-{
+const server::Options &Config::get_server() const {
 	return (this->_serv_opts);
 }
 
-void Config::_tokenizer(const std::string &line)
-{
+void Config::_tokenizer(const std::string &line) {
 	std::string token;
 	size_t i = 0;
 	size_t size = line.length();
 	char symbol;
 
-	while (i < size)
-	{
+	while (i < size) {
 		symbol = line[i];
-		switch (symbol)
-		{
+		switch (symbol) {
 			case ' ':
 			case '\n':
 			case '\t':
-			case '\0':
-			{
-				if (!token.empty())
-				{
+			case '\0': {
+				if (!token.empty()) {
 					_tokens.push_back(token);
 					token.clear();
 				}
@@ -59,10 +49,8 @@ void Config::_tokenizer(const std::string &line)
 			}
 			case '{':
 			case '}':
-			case ';':
-			{
-				if (!token.empty())
-				{
+			case ';': {
+				if (!token.empty()) {
 					_tokens.push_back(token);
 					token.clear();
 				}
@@ -76,26 +64,22 @@ void Config::_tokenizer(const std::string &line)
 	}
 }
 
-const std::string Config::_readFile(const char *filename)
-{
+const std::string Config::_readFile(const char *filename) {
 	std::string line;
 	std::string buf;
 	std::string::size_type start_comment = 0;
 	std::string::size_type end_comment = 0;
 
 	std::ifstream input_file(filename);
-	if (!input_file.is_open())
-	{
+	if (!input_file.is_open()) {
 		throw config::Config::FileNotFoundException();
 	}
-	while (std::getline(input_file, buf))
-	{
+	while (std::getline(input_file, buf)) {
 		buf += '\n';
 		line += buf;
 	}
 	input_file.close();
-	while (start_comment != std::string::npos)
-	{
+	while (start_comment != std::string::npos) {
 		start_comment = line.find_first_of('#');
 		end_comment = line.find_first_of('\n', start_comment);
 		if (start_comment != std::string::npos)
@@ -104,16 +88,14 @@ const std::string Config::_readFile(const char *filename)
 	return (line);
 }
 
-const std::vector<std::string> Config::_lexing(const std::string &filename)
-{
+const std::vector<std::string> Config::_lexing(const std::string &filename) {
 	_log.debug("start processing a file: " + filename);
 	_log.debug("lexing: read all file, delete comments  and splitting words into tokens");
 	_tokenizer(_readFile(filename.c_str()));
 	return _tokens;
 }
 
-void Config::_parsing(const std::vector<std::string> &tokens)
-{
+void Config::_parsing(const std::vector<std::string> &tokens) {
 	std::vector<std::string>::const_iterator it = tokens.begin();
 
 	_log.debug("start parsing tokens into global module structure");
@@ -123,8 +105,7 @@ void Config::_parsing(const std::vector<std::string> &tokens)
 	_log.debug("splitting directives and modules");
 }
 
-Config::Directive Config::_collect_directive(const std::vector<std::string> &tokens) const
-{
+Config::Directive Config::_collect_directive(const std::vector<std::string> &tokens) const {
 	Config::Directive dir;
 
 	if (tokens.empty())
@@ -136,8 +117,7 @@ Config::Directive Config::_collect_directive(const std::vector<std::string> &tok
 }
 
 Config::Module Config::_collect_module(const std::vector<std::string> &name, const std::vector<std::string> &tokens,
-									   std::vector<std::string>::const_iterator &it, bool is_global) const
-{
+									   std::vector<std::string>::const_iterator &it, bool is_global) const {
 	Config::Module local_module;
 
 	if (name.empty())
@@ -146,15 +126,12 @@ Config::Module Config::_collect_module(const std::vector<std::string> &name, con
 	local_module.name = name[0];
 	local_module.args.assign(name.begin() + 1, name.end());
 	std::vector<std::string> tokens_accumulator;
-	for (; it != tokens.end() && (*it != "}" || is_global); ++it)
-	{
-		if (*it == ";")
-		{
+	for (; it != tokens.end() && (*it != "}" || is_global); ++it) {
+		if (*it == ";") {
 			local_module.directives.push_back(_collect_directive(tokens_accumulator));
 			_log.debug("filled directive " + tokens_accumulator[0]);
 			tokens_accumulator.clear();
-		} else if (*it == "{")
-		{
+		} else if (*it == "{") {
 			local_module.modules.push_back(_collect_module(tokens_accumulator, tokens, ++it, false));
 			_log.debug("filled module " + tokens_accumulator[0]);
 			tokens_accumulator.clear();
@@ -166,23 +143,19 @@ Config::Module Config::_collect_module(const std::vector<std::string> &name, con
 	return local_module;
 }
 
-void Config::_fill_options(const Config::Module &global_module)
-{
+void Config::_fill_options(const Config::Module &global_module) {
 	(void) global_module;
 	_log.debug("start filling configuration staff in to logger and server options");
 }
 
-const char *Config::FileNotFoundException::what() const throw()
-{
+const char *Config::FileNotFoundException::what() const throw() {
 	return "Config file was not found";
 }
 
-const char *Config::ReadingConfigFileException::what() const throw()
-{
+const char *Config::ReadingConfigFileException::what() const throw() {
 	return "Error while reading config file";
 }
 
-const char *Config::ParsingErrorException::what() const throw()
-{
+const char *Config::ParsingErrorException::what() const throw() {
 	return "parsing error";
 }
--- srcs/config/Config.hpp
+++ srcs/config/Config.hpp
@@ -18,115 +18,124 @@ namespace config {
 	// Config class responsible for parsing of a configuration file and returning options
 	// of other classes of the program
 	class Config : public IConfig {
-		private:
-			// Directive data structure for "directive" in configuration file
-			// example:
-			//   Directive{ name: "error_log", args: {"/dev/stderr","warn"} }
-			struct Directive {
-				std::string              name;
-				std::vector<std::string> args;
-			};
-			// Module data structure for "module", "block" or "context" in configuration file
-			// example:
-			//   Module{
-			//     name: "main",
-			//     directive: {},
-			//     modules{
-			//       Module{
-			//         name: "http",
-			//         directive: {},
-			//         modules{ Module{name: "server", ...}, Module{name: "server", ...} }
-			//       }
-			//     }
-			//   }
-			struct Module {
-				std::string            name;
-				std::vector<Module>    modules;
-				std::vector<Directive> directives;
-				std::vector<std::string> args;
-			};
-		private:
-			// _serv_opts options of a Server, Server is a class of the program
-			// which is responsible for network connection and HTTP protocol
-			server::Options _serv_opts;
-			// _log_opts options of a Logger, Logger is a class of the program
-			// which is responsible for printing or recording information about
-			// progress of the program
-			logger::Options _log_opts;
+	private:
+		// Directive data structure for "directive" in configuration file
+		// example:
+		//   Directive{ name: "error_log", args: {"/dev/stderr","warn"} }
+		struct Directive {
+			std::string name;
+			std::vector<std::string> args;
+		};
+		// Module data structure for "module", "block" or "context" in configuration file
+		// example:
+		//   Module{
+		//     name: "main",
+		//     directive: {},
+		//     modules{
+		//       Module{
+		//         name: "http",
+		//         directive: {},
+		//         modules{ Module{name: "server", ...}, Module{name: "server", ...} }
+		//       }
+		//     }
+		//   }
+		struct Module {
+			std::string name;
+			std::vector<Module> modules;
+			std::vector<Directive> directives;
+			std::vector<std::string> args;
+		};
+	private:
+		// _serv_opts options of a Server, Server is a class of the program
+		// which is responsible for network connection and HTTP protocol
+		server::Options _serv_opts;
+		// _log_opts options of a Logger, Logger is a class of the program
+		// which is responsible for printing or recording information about
+		// progress of the program
+		logger::Options _log_opts;
 
-			const logger::Logger &_log;
+		const logger::Logger &_log;
 
-			std::vector<std::string> _tokens;
-		public:
-			Config(const logger::Logger &log);
-			~Config();
+		std::vector<std::string> _tokens;
+	public:
+		Config(const logger::Logger &log);
 
-			// parse take name of configuration file and read it
-			// fill _serv_opts and _log_opts according content of the file
-			// if syntax of the file is bad, throw exception
-			void parse(const std::string& filename);
+		~Config();
 
-			// get_logger get Logger options
-			const logger::Options& get_logger() const;
-			// get_server get Server options
-			const server::Options& get_server() const;
-		public:
-			// TODO make it better (more verbose)
+		// parse take name of configuration file and read it
+		// fill _serv_opts and _log_opts according content of the file
+		// if syntax of the file is bad, throw exception
+		void parse(const std::string &filename);
 
-			struct FileNotFoundException : public std::exception {
-            	const char* what () const throw ();
-            };
+		// get_logger get Logger options
+		const logger::Options &get_logger() const;
 
-            struct ReadingConfigFileException: public std::exception {
-                const char* what () const throw ();
-            };
-			// TODO make it better (more verbose)
-			struct ParsingErrorException : public std::exception {
-				const char* what () const throw ();
-			};
-		private:
-			// _lexing read configuration file by it name ignoring strings which start
-			// with "#" (comments) and splits received text on tokens
-			//   example:
-			//   ```
-			//   # Some comment
-			//   daemon off;
-			//   events {
-			//   }
-			//   ``` -> {"daemon", "off", ";", "events", "{", "}"}
-			// May throw exception in case of bad syntax of configuration file
-			//   example of bad syntax: "daemon off;;", "events {{ }", "events {; }"
-			//   (wrong token sequence)
-			const std::vector<std::string> _lexing(const std::string& filename);
+		// get_server get Server options
+		const server::Options &get_server() const;
 
-			const std::string	    _readFile(const char *filename);
+	public:
+		// TODO make it better (more verbose)
 
-			void	                _tokenizer(const std::string &line);
-			// _parsing sort tokens into Module structure
-			//   example:
-			//     {"daemon", "off", ";", "events", "{", "}"}
-			//   ->
-			//     Module{
-			//       name: "main",
-			//       directives: { Directive{name:"", args:{"off"}} },
-			//       modules: {
-			//         Module {name: "events", directives: {}, modules: {}}
-			//       }
-			//     }
-			// May throw exception in case of bad syntax
-			//   example of bad syntax: {"daemon", "off", ";", "events", "{"}
-			//   (don't close curly bracket)
-			void                   _parsing(const std::vector<std::string>& tokens);
+		struct FileNotFoundException : public std::exception {
+			const char *what() const throw();
+		};
+
+		struct ReadingConfigFileException : public std::exception {
+			const char *what() const throw();
+		};
+
+		// TODO make it better (more verbose)
+		struct ParsingErrorException : public std::exception {
+			const char *what() const throw();
+		};
+
+	private:
+		// _lexing read configuration file by it name ignoring strings which start
+		// with "#" (comments) and splits received text on tokens
+		//   example:
+		//   ```
+		//   # Some comment
+		//   daemon off;
+		//   events {
+		//   }
+		//   ``` -> {"daemon", "off", ";", "events", "{", "}"}
+		// May throw exception in case of bad syntax of configuration file
+		//   example of bad syntax: "daemon off;;", "events {{ }", "events {; }"
+		//   (wrong token sequence)
+		const std::vector<std::string> _lexing(const std::string &filename);
+
+		const std::string _readFile(const char *filename);
+
+		void _tokenizer(const std::string &line);
+
+		// _parsing sort tokens into Module structure
+		//   example:
+		//     {"daemon", "off", ";", "events", "{", "}"}
+		//   ->
+		//     Module{
+		//       name: "main",
+		//       directives: { Directive{name:"", args:{"off"}} },
+		//       modules: {
+		//         Module {name: "events", directives: {}, modules: {}}
+		//       }
+		//     }
+		// May throw exception in case of bad syntax
+		//   example of bad syntax: {"daemon", "off", ";", "events", "{"}
+		//   (don't close curly bracket)
+		void _parsing(const std::vector<std::string> &tokens);
 
 	private:
 		// todo: find better name for this method
-			Directive				_collect_directive(const std::vector<std::string>& tokens) const;
-			Module					_collect_module(const std::vector<std::string>& name, const std::vector<std::string>& tokens, std::vector<std::string>::const_iterator& it, bool is_global) const;
-			// _fill_options sort data from Module structure to options of other classes
-			// will be expend during the progress of creation program
-			// May throw exception in case of bad directive or module name,
-			// bad module nesting or bad arguments in directive
-			void                    _fill_options(const Module& global_module);
+		Directive _collect_directive(const std::vector<std::string> &tokens) const;
+
+		Module _collect_module(const std::vector<std::string> &name, const std::vector<std::string> &tokens,
+							   std::vector<std::string>::const_iterator &it, bool is_global) const;
+
+		// _fill_options sort data from Module structure to options of other classes
+		// will be expend during the progress of creation program
+		// May throw exception in case of bad directive or module name,
+		// bad module nesting or bad arguments in directive
+		void _fill_options(const Module &global_module);
 
 	private:
 		Module _global_module;
