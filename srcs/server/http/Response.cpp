8113cea9250ef65d0b875c0091179cbd580f6108
Nastya <hurmuzz23@gmail.com>
2022-04-25 22:54:15 +0300

    class definition in  progress

--- srcs/server/http/Response.cpp
+++ srcs/server/http/Response.cpp
@@ -10,3 +10,24 @@ const std::string Response::serialize() const {
 		"\r\n"
 		"Hello world!";
 }
+
+std::strign Response::_create_status_line() const {
+	std::ostringstream _str;
+
+	_str << "HTTP/" << _protocol_version / 10 << "." << _protocol_version % 10 << " ";
+	_str << _status_code << " " << _convert_status_code_to_string << std::endl;
+	return _str.str();
+}
+
+
+std::string Response::_convert_status_code_to_string() const {
+	// todo: compare code for http 1.1 and other versions
+	switch (_status_code)
+	{
+		case 200:
+			return "OK";
+		case 404:
+		default:
+			return "Not Found";
+	}
+}
--- srcs/server/http/Response.hpp
+++ srcs/server/http/Response.hpp
@@ -2,8 +2,23 @@
 #define RESPONSE_HPP
 
 #include <string>
+#include <sstream>
 
 namespace http {
+	enum ConnectionStatus {
+		keepAlive,
+		close
+	};
+
+	enum ContentType {
+		text,
+		image,
+		audio,
+		video,
+		// other types
+	};
+
+#define CONTENT_TYPE_TO_TEXT(type) #type;
 
 class Response {
 public:
@@ -11,6 +26,29 @@ public:
   const std::string serialize() const;
 
 private:
+	std::strign _create_status_line() const;
+	std::string _create_header() const;
+	std::string _create_body() const;
+	std::string _convert_status_code_to_string() const;
+
+private:
+	/// status line
+	int _protocol_version;
+	int _status_code;
+
+	/// general headers
+	http::ConnectionStatus _connection;
+	std::tm*	_date;
+
+	/// response headers
+
+	/// entity headers
+	size_t _length;
+	// content type
+	int _type; // ?
+	int _subtype; // ?
+
+	/// message body
 };
 
 } // namespace http
